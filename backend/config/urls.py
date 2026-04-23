5daf8b9adf37f6dc86509331ba32df9a8e9159c5
Aziz Rahmad <rahmadaziz288@gmail.com>
2026-04-23 20:52:57 +0700

    Add backend

---
+++ backend/config/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+router = DefaultRouter()
+# router.register(r'budgets', BudgetViewSet, basename='budget')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/', include('expensapp_auth.urls')),
+
+
+    path('api/docs/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    path('api-auth/', include('rest_framework.urls')),
+]
